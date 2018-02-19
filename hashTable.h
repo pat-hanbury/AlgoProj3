50b53217f946ad3dd5774687cac3313594e887a3
jmkang240 <35613765+jmkang240@users.noreply.github.com>
2018-02-19 17:43:58 -0500

    implement hash table and heap file for heapsort

---
+++ hashTable.h
@@ -0,0 +1,93 @@
+//John Kang and Patrick Hanbury
+//Fundamental of Algorithms
+//Project3b
+//hashTable.h
+
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <functional>
+#include <vector>
+
+//hashTable template declarataion
+template <typename T> class hashTable{ 
+//private members
+private:
+	//Initializes hashData object that returns key for every element
+	std::hash<T> hashData; 
+	//Initiazlies Table to store vectors
+	std::vector<std::vector<T> > table; 
+	//Initiazlies the number of rows in hash Table
+	int numRows; 
+
+//public members
+public:
+	//hash function that takes item and returns the hash value of the item
+	int hash(T &item){
+		//Uses the hashData object and uses mod to find remainder from numRows
+		//Hash value of the item is simply a number from 0 to numRows. 
+		return hashData(item) % numRows; 
+	}
+	
+	//void function initializes the hash table and takes in a type T vector
+	void initializeHashTable(std::vector<T> &list){ 
+		//Initialize number of rows to 3 collisions per row.
+		numRows = floor(list.size() / 3); 
+		//Initializes number of rows in the hash Table
+		table.resize(numRows); 
+		//for statement that iterates over the entire list.
+		for (int i=0; i<list.size(); i++){ 
+			//calls addItem function to add an item in the hash Table
+			addItem(list.at(i)); 
+		}
+	}
+	
+	//void function that adds an item to the hash Table
+	void addItem(T &item){ 
+		//Initializes integer key for the item
+		int key; 
+		//Sets the key to the item using function hash() to return hash value for the item
+		key = hash(item); 
+		//push back function to insert the item into the hash Table.
+		table[key].push_back(item); 
+	}
+
+	//void function that deletes an item from the hash Table
+	void deleteItem(T &item){
+		//Initializes integer key for the item using function hash()
+		int key = hash(item); 
+		
+		//for statement that iterates over the the entire list.
+		for (int i=0; i < table[key].size(); i++){
+			//if statement that checks to see if the key at i finds the item  
+			if (table[key].at(i) == item){
+				//Initializes a temp variable to set the key at i
+				std::string temp = table[key].at(i); 
+				//Swaps the item with the last item in the row
+				table[key].at(i) = table[key].at(table[key].size() - 1);
+				//sets the last item in the row to the temp variable
+				table[key].at(table[key].size() - 1) = temp;
+				//pop back function to delete the item form the row
+				table[key].pop_back();
+				return; 
+			}
+		}
+	}
+
+	//bool function that checks to see if item is found
+	bool inList(T &item){
+		//Initializes integer key for the item using the function hash()
+		int key = hash(item); 
+		
+		//for statement that iterates over the entire hash row
+		for (int i=0; i<table[key].size(); i++){ 
+			//if statement that checks to see if the key at i finds the item
+			//returns true if found, return false if not.
+			if (table[key].at(i) == item) 
+				return true; 
+		}
+		return false; 
+	}
+};
+
+#endif 
---
+++ heap.h
@@ -0,0 +1,192 @@
+//John Kang and Patrick Hanbury
+//Fundamental of Algorithms
+//Project3b
+//heap.h
+
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <iostream>
+#include <vector>
+
+//heap declaration
+template <typename T> class heap{
+	
+//private members
+private: 
+	//heap vector to store the heap
+	std::vector<T> heapArray;
+	//heap string to store the type of the heap
+	std::string heapType;
+	//heap variable to store the number of elements
+	int heapSize;
+
+//public members	
+public:
+	//overloaded print operator to print the heap
+	friend std::ofstream& operator << (std::ofstream &out, heap<std::string> &list); 
+
+	//Initialization of the integer parent of the heap.
+	//Parent of heap
+	int parent(int i){ 
+		return floor(i / 2);
+	}
+	
+	//Initialization of the integer left of the heap.
+	//Left child of heap
+	int left(int i){ 
+		return 2 * (i + 1) -1;
+	}
+
+	//Initialization of the integer right of the heap.
+	//Right child of heap
+	int right(int i){ 
+		return 2 * (i + 1);
+	}
+
+	//Initialization of getItem function that returns the nth item in the heap. 
+	//Returns an object that is part of the stored heap vector, heapArray.
+	T getItem(int n) { 
+		return heapArray.at(n);
+	}
+
+	//Void function that initializes MaxHeap
+	void initializeMaxHeap(std::vector<T> arr){ 
+		//Initializes variable heapSize to 0
+		heapSize = 0;
+		//for loop that iterates over the size of the array
+		//Push back function to put i at back of the array
+		//Increments the size of the heap
+		for (int i = 0; i < arr.size(); i++){ 
+			heapArray.push_back(arr.at(i)); 
+			heapSize++; 
+		}
+		//Names heapType of Max
+		heapType = "Max Heap"; 
+		//Calls member function to buildMaxHeap
+		buildMaxHeap(); 
+	}
+	
+	//Void function that heapifies a maxHeap, takes in integer i.
+	void maxHeapify(int i){ 
+		//Initialization of integers largest to store index of largest element. 
+		//Initialization of integer left to store left child
+		//Initialization of integer right to right child.
+		int largest; 
+		int Left = left(i); 
+		int Right = right(i); 
+		
+		//If statement that compares left child to the current element. 
+		if (Left < heapSize && heapArray.at(Left) > heapArray.at(i)) 
+			//Index of the largest element is the left child
+			largest = Left; 
+		//Else statement that will store index of current element in largest
+		else largest = i; 
+		
+		//if statement that compares right child to the current element, and greater than largest
+		if (Right < heapSize && heapArray.at(Right) > heapArray.at(largest)) 
+			//Index of the largest element is the right child
+			largest = Right; 
+			
+		//if statement that checks if largest is not the current element.
+		if (largest != i) { 
+			//temp variable that checks for heap array.
+			//Swaps largest element with current element
+			T temp = heapArray.at(i); 
+			heapArray.at(i) = heapArray.at(largest);
+			heapArray.at(largest) = temp;
+			//Calls maxHeapify function with the largest element as the current element,
+			maxHeapify(largest);
+		}
+	}
+
+	//void function buildMaxHeap that creates the max heap
+	void buildMaxHeap(){ 
+		//for statement that decrements the size of the heap by one.
+		for (int i = floor((heapSize - 1) / 2); i >= 0; i--){ 
+			//Calls max heapify for i.
+			maxHeapify(i); 
+		}
+	}
+
+	//void function initialization that operates similarly to initializationMaxHeap.
+	void initializeMinHeap(std::vector<T> arr){
+		//Initializes variable heapSize to 0
+		heapSize = 0; 
+		//for loop that iterates over the size of the array
+		//Push back function to put i at back of the array
+		//Increments the size of the heap
+		for (int i = 0; i < arr.size(); i++){ 
+			heapArray.push_back(arr.at(i)); 
+			heapSize++;
+		}
+		//Names heapType of Min
+		heapType = "Min Heap"; 
+		//Calls member function to buildMinHeap
+		buildMinHeap(); 
+	}
+
+	//void function that heapifies a minHeap, takes in integer i.
+	void minHeapify(int i){ 
+		//Initialization of integers smallest to store index of smallest element. 
+		//Initialization of integer left to store left child
+		//Initialization of integer right to right child.
+		int smallest; 
+		int Left = left(i); 
+		int Right = right(i); 
+		
+		//If statement that compares left child to the current element. 
+		if (Left < heapSize && heapArray.at(Left) < heapArray.at(i)) 
+			//Index of the smallest element is the left child
+			smallest = Left; 
+		//Else statement that will store index of current element in smallest
+		else smallest = i; 
+		
+		//if statement that compares right child to the current element, and less than lsmallest
+		if (Right < heapSize && heapArray.at(Right) < heapArray.at(smallest))
+			//Index of the smallest element is the right child
+			smallest = Right; 
+		
+		//if statement that checks if smallest is not the current element.
+		if (smallest != i){
+			//variable that checks for heap array.
+			//Swaps smallest element with current element
+			T temp = heapArray.at(i); 
+			heapArray.at(i) = heapArray.at(smallest);
+			heapArray.at(smallest) = temp;
+			//Calls mainHeapify function with the smallest element as the current element,
+			minHeapify(smallest); 
+		}
+	}
+
+	////void function buildMinHeap that creates the min heap
+	void buildMinHeap() { 
+		//for statement that decrements the size of the heap by one.
+		for (int i = floor((heapSize - 1) / 2); i > 0; i--){ 
+			//Calls min heapify for i.
+			minHeapify(i); 
+		}
+	}
+
+	//void function that heapSorts a maxheap
+	void heapSort(){ 
+		//Stores current size of heap
+		int heapSizeStore = heapSize; 
+		//for statement that iterates over the entire heap 
+		for (int i = heapSize - 1; i > 0; i--){ 
+			//variable that checks for heap array.
+			//Swaps first element with current element
+			T temp = heapArray.at(i); 
+			heapArray.at(i) = heapArray.at(0);
+			heapArray.at(0) = temp;
+			//Decements heapsize.
+			heapSize--; 
+			//Calls maxHeapify because we want the heap to be smaller than what it was before
+			maxHeapify(0); 
+		}
+		//Resets the heapSize to the previously stored size, 
+		heapSize = heapSizeStore; 
+	}
+};
+
+#endif
